bc0f316d71accde14e822d7977039d91abe9df01
vaibhavgoyal212 <s2106809@ed.ac.uk>
2022-12-04 02:39:50 +0000

    final submmission

--- src/main/java/uk/ac/ed/inf/DroneMove.java
+++ src/main/java/uk/ac/ed/inf/DroneMove.java
@@ -1,11 +1,32 @@
 package uk.ac.ed.inf;
 
 public class DroneMove {
+    /**
+     * field for the order number associated with the move
+     */
     private String orderNo;
+
+    /**
+     * field for the current location of the drone
+     */
     private LngLat position;
+    /**
+     * field for the location of the parent (starting point of move)
+     */
     private LngLat parent;
+    /**
+     * field for the angle that the drone has moved from the parent to the current position
+     */
     private Double angle;
+
+    /**
+     * field for the ticks elapsed since the start of the path calculation
+     */
     private long ticksSinceStartOfCalculation;
+
+    /**
+     * field to check if the move is a starting move(in which case the parent is null)
+     */
     private boolean isStartMove;
 
     public DroneMove(Node node){
